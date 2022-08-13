4c30ccaa9bef1124410afcd710d45f333c642f36
yaseminaktass <yasemin.aktas@mdpgroup.com>
2022-08-13 13:32:30 +0300

    The Project Continues.

--- CoreDemo/Controllers/BlogController.cs
+++ CoreDemo/Controllers/BlogController.cs
@@ -8,13 +8,12 @@ using System.Threading.Tasks;
 
 namespace CoreDemo.Controllers
 {
-    public class CategoryController : Controller
+    public class BlogController : Controller
     {
-
-        CategoryManager cm = new CategoryManager(new EfCategoryRepository());
+        BlogManager bm = new BlogManager(new EfBlogRepository());
         public IActionResult Index()
         {
-            var values = cm.GetList();
+            var values = bm.GetList();
             return View(values);
         }
     }
